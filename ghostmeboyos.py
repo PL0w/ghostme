b2e79f7c6607fdc2fc1cf307b0d915597654831b
Brannon <youngbrann@gmail.com>
2023-01-26 23:32:55 -0500

    added exceptions

--- ghostmeboyos.py
+++ ghostmeboyos.py
@@ -2,7 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, ElementNotVisibleException, NoSuchElementException
+from selenium.common.exceptions import ElementClickInterceptedException
 import time
 import os # to clear screen
 
@@ -25,12 +26,6 @@ def checkVisibility(elem):
 def checkClickability(elem):
     return WebDriverWait(browser, 10).until(EC.element_to_be_clickable(elem))
 
-# def checkThreadStatus(url):
-#     thread_status = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.CLASS_NAME, 'newcontent_textcontrol')))
-#     if thread_status == 'Closed Thread':
-#         return False
-#     else: True
-
 ################# START #################
 
 
@@ -38,6 +33,7 @@ def checkClickability(elem):
 ################# LOGIN #################
 
 username = password = False
+# Enter user/pass
 while not (username and password):
     os.system('cls')
     displayLogo()
@@ -45,51 +41,58 @@ while not (username and password):
     password = input('Enter password: ')
     print('')
 
+# Create Firefox object
 browser = webdriver.Firefox()
 site = 'https://www.bodybuilding.com/combined-signin?referrer=https%3A%2F%2Fforum.bodybuilding.com%2F%23&country=US'
 browser.get(site)
 
+# Search for username field, input username
 try: 
     user_field = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.NAME, 'username')))
     if user_field:
         user_field.send_keys(username)
-except: pass
+except ElementNotVisibleException: print('Username field not found...')
 
+# Search for password field, input password
 try:
     pass_field = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.NAME, 'password')))
     if pass_field:
         pass_field.send_keys(password)
-except: pass
+except ElementNotVisibleException: print('Password field not found...')
 
 
 # Click off "want 15% off?" popup
-i = 'N'
-while i != 'Y':
-    i = input('\nDid you remove 15% off popup? (Y/N) ')
+ugh = 'N'
+while ugh != 'Y':
+    ugh = input('\nDid you remove 15% off popup? (Y/N) ')
 
 
-try:
-    fifteenoff = WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.CLASS_NAME, 'ab-programmatic-close-button')))
-except: pass
-
+# try:
+#     fifteenoff = WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.CLASS_NAME, 'ab-programmatic-close-button')))
+# except: pass
 # didclose = input('did id close?')
 
+# Click 'sign-in' button
 try:
     login = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'combined-sign-in--button'))).click()
-except: pass
+except ElementClickInterceptedException: print('Sign-in not reached...')
 
+# Wait page load
 time.sleep(3)
 
 ################# USERID #################
 
+# Load BB.com dark theme
 dark_mode = 'https://forum.bodybuilding.com/?styleid=63'
 browser.get(dark_mode)
 
+# Checks for user id (determines login success)
 try:
     id1 = WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.CLASS_NAME, 'welcomelink [HREF]')))
     print('Login successful...')
-except: print('Not logged in...')
+except NoSuchElementException: print('Login unsuccessful...')
 
+# Split id from link
 id2 = id1.get_attribute('href')
 id3 = id2.split('=')
 user_id = id3[1]
@@ -116,7 +119,8 @@ except TimeoutException:
 
 # stats
 posts_deleted = 0
-# page = 0
+
+################# PAGES + THREAD #################
 
 while thread_links_elem:
 
@@ -151,19 +155,23 @@ while thread_links_elem:
         
         try: # edits post for del. targ -> "vB::QuickEdit::" + post ID
             edit_post = WebDriverWait(browser, 60).until(EC.element_to_be_clickable((By.NAME, z))).click()
-        except: pass
+        except ElementClickInterceptedException: 
+            print('Unable to click edit button...')
 
         try: # deletes post
             delete_post = WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.ID, 'vB_Editor_QE_1_delete'))).click() 
-        except: pass
-        
+        except ElementClickInterceptedException: 
+            print('Unable to click delete button...')
+
         try: # toggle 'delete message radio button'
             dlt_radio_toggle = WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.CLASS_NAME, 'dep_ctrl'))).click() 
-        except: pass
+        except ElementClickInterceptedException: 
+            print('Unable to click toggle delete button...')
 
         try: # confirm delete
             confirm_delete = WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.ID, 'quickedit_dodelete'))).click()
-        except: pass
+        except ElementClickInterceptedException: 
+            print('Unable to click confirm delete button...')
         
         posts_deleted += 1
         print(f'[{posts_deleted}] posts deleted')
@@ -178,14 +186,17 @@ while thread_links_elem:
         next_page_list = WebDriverWait(browser, 30).until(EC.visibility_of_all_elements_located((By.CLASS_NAME, 'prev_next [href]')))
         next_page = next_page_list[1]
         next_page.click()
-    except: pass
+    except ElementNotVisibleException: 
+        print('Next page not found...')
+        input('Press enter to close program.\n')
+        break
 
     # update page num
     post_history_page_num = str(int(post_history_page_num) + 1)
     
     try: # get title links
         thread_links_elem = WebDriverWait(browser, 30).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'posttitle [href]')))
-    except TimeoutException: 
+    except NoSuchElementException: 
         print('\nPost history not found...')
-        input('\nPress enter to close program.')
+        input('\nPress enter to close program.\n')
         break
